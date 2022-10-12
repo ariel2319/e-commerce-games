c2e58e7d1cbb7e059801a066c3119feba20374d3
areil <maldonadoaa.91@gmail.com>
2022-10-12 09:17:58 -0300

    fills funcionando

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //mis datos
-const games =[
+const gamesList =[
   { id: 1,
     name: 'A Plague Tale',
     img: './images/a plague tale portada.jpg',
@@ -33,7 +33,7 @@ const games =[
     img: './images/nba 2k 23 portada.jpg',
     price: 59.9,
     amount: 5, 
-    discount: 0,
+    discount: 0, 
   },
   { id: 6,
     name: 'Red Dead Redeption II',
@@ -99,3 +99,64 @@ const games =[
     discount: 50,
   },
 ]
+
+/* Cargar juegos STORE */
+const gamesStore = document.querySelector('.games-container');
+
+function fillStore () {
+  let html='';
+  for (const {id, name, img, price, amount, discount} of gamesList){
+    if (discount === 0){
+      html += `
+      <article class="store_card">   
+        <img class="store_img" src="${img}" alt="${name}">
+        <div class="store_card_description">
+          <p class="store_card_title">${name}</p>
+          <button class="store_button_add" id="btn-add">
+            <span class="material-symbols-outlined">
+            add_circle
+            </span>
+          </button>
+          <p class="store_card_price">$ ${price}</p>
+          <p class="store_card_amount">${amount} U.</p>
+        </div>
+    </article>
+    `
+    }
+  }
+  gamesStore.innerHTML = html;
+}
+fillStore();
+
+/* Cargar juegos SALE */
+const gamesSale = document.querySelector('.games-sale-container');
+
+function fillSale () {
+  let html = '';
+  for (const{id, name, img, price, amount, discount} of gamesList){
+    if (discount !== 0){
+      const priceDiscount = price - (price * discount / 100);
+      html += `
+      <article class="sale_card">
+        <img class="sale_img" src="${img}" alt="${name}">
+        <div class="sale_card_description">
+          <p class="sale_card_title">${name}</p>
+          <button class="sale_button_add">
+            <span class="material-symbols-outlined">
+            add_circle
+            </span>
+          </button>
+          <div class="sale_card_discount_container">
+            <p class="sale_card_discount">%${discount}</p>
+          </div>
+          <p class="sale_card_old_price">$ ${price}</p>
+          <p class="sale_card_price">$${priceDiscount.toPrecision(4)}</p>
+          <p class="sale_card_amount">${amount} U.</p>
+        </div>
+      </article>
+      `
+    }
+  }
+  gamesSale.innerHTML = html;
+}
+fillSale();
